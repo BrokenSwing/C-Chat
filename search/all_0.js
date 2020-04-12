13439cf76d0c5b678606b0ed2c945b735ee90e8d
BrokenSwing <BrokenSwing@users.noreply.github.com>
2020-04-12 15:00:46 +0000

    deploy: 65fbbc49685f3e749dd2cc825caa6a6028f67f03

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['socketinfo_0',['SocketInfo',['../struct_socket_info.html',1,'']]]
+];
---
+++ search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['socketinfo_1',['SocketInfo',['../struct_socket_info.html',1,'']]]
+];
