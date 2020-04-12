a343259ee92c5190d47e855ed34732dda00eca86
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-04-12 16:17:35 +0200

    Now relay messages between two clients

--- serveur.c
+++ serveur.c
@@ -1,24 +1,65 @@
 #include "sockets.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <signal.h>
+
+#define MSG_MAX_LENGTH 250
+
+void handleServerClose(int signal) {
+    cleanUp();
+    printf("Server closed.\n");
+    exit(EXIT_SUCCESS);
+}
+
+int receivedEndMessage(const char* buffer) {
+    return buffer[0] == 'f' && buffer[1] == 'i' && buffer[2] == 'n' && buffer[3] == '\0';
+}
 
 int main () {
     SocketInfo serverSocket = createServerSocket("27015");
+    signal(SIGINT, handleServerClose);
 
-    SocketInfo clientSocket = acceptClient(serverSocket);
+    char buffer[MSG_MAX_LENGTH + 1];
 
-    char buffer[250];
-    int bytesReceived = receiveFrom(clientSocket, buffer, 249);
-    buffer[bytesReceived] = '\0';
-    printf("Received %d bytes. Message : %s\n", bytesReceived, buffer);
+    do {
+        printf("Waiting for first client to connect.\n");
+        SocketInfo receiver = acceptClient(serverSocket);
+        sendTo(receiver, "1", 1);
+        printf("Waiting for second client to connect.\n");
+        SocketInfo sender = acceptClient(serverSocket);
+        sendTo(sender, "2", 1);
 
-    const char* answer = "Got it!";
-    sendTo(clientSocket, answer, strlen(answer));
+        printf("Two clients connected !\n");
 
-    closeSocket(clientSocket);
+        do {
+            int bytesCount = receiveFrom(sender, buffer, MSG_MAX_LENGTH);
+            if (bytesCount == 0) { // Connection with sender is lost
+                closeSocket(receiver);
+                printf("Connection interrupted by sender.\n");
+                break;
+            }
+            buffer[bytesCount] = '\0';
 
-    closeSocket(serverSocket);
-    cleanUp();
-    return EXIT_SUCCESS;
+            if (receivedEndMessage(buffer)) {
+                printf("A client want to leave. Closing sockets.\n");
+                closeSocket(sender);
+                closeSocket(receiver);
+                break;
+            }
+
+            bytesCount = sendTo(receiver, buffer, bytesCount);
+            if (bytesCount == 0) { // Connection with receiver is lost
+                closeSocket(sender);
+                printf("Connection interrupted by receiver.\n");
+                break;
+            }
+
+            printf("Relayed message : %s\n", buffer);
+
+            SocketInfo tmp = sender;
+            sender = receiver;
+            receiver = tmp;
+
+        } while(1);
+    } while(1);
 }
