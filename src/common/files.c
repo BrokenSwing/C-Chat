f12f5d9dc92f01e1eb35906eb892278b744a3a64
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-05-01 16:28:08 +0200

    Add read and write function for WIndows ONLY
    Need POSIX implementation

--- src/common/files.c
+++ src/common/files.c
@@ -1,5 +1,6 @@
 #include "files.h"
 #include "interop.h"
+#include <stdio.h>
 
 #if IS_POSIX
 
@@ -11,6 +12,7 @@ FileInfo files_getInfo(const char* filename) {
 
     FileInfo info;
     if (stat(filename, &st) == 0) {
+        info.exists = 1;
         info.isDirectory = S_ISDIR(st.st_mode);
         if (info.isDirectory) {
             info.size = st.st_size;
@@ -18,6 +20,7 @@ FileInfo files_getInfo(const char* filename) {
             info.size = 0;
         }
     } else {
+        info.exists = 0;
         info.isDirectory = 0;
         info.size = 0;
     }
@@ -35,17 +38,71 @@ FileInfo files_getInfo(const char* filename) {
     GetFileAttributesEx(filename, GetFileExInfoStandard, &wInfo);
 
     FileInfo info;
+    info.exists = 1;
     info.isDirectory = (wInfo.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) > 0;
     if (!info.isDirectory) {
         LARGE_INTEGER size;
         size.LowPart = wInfo.nFileSizeLow;
         size.HighPart = wInfo.nFileSizeHigh;
         info.size = size.QuadPart;
+        if (info.size < 0) {
+            info.size = 0;
+            info.exists = 0;
+        }
     } else {
         info.size = 0;
     }
 
     return info;
+}
+
+unsigned long files_readFile(const char* filename, char* contentBuffer, unsigned long bufferSize) {
+    FileInfo info = files_getInfo(filename);
+    if (info.isDirectory) {
+        return -1;
+    }
+    HANDLE file = CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+        printf("Unable to open file: %s\n", filename);
+        CloseHandle(file);
+        return -1;
+    }
+
+    unsigned long readCount;
+    if(!ReadFile(file, contentBuffer, bufferSize, &readCount, NULL)) {
+        printf("Unable to read file: %s\n", filename);
+        printf("Error code: %ld\n", GetLastError());
+        CloseHandle(file);
+        return -1;
+    }
+    CloseHandle(file);
+
+    return readCount;
+}
+
+unsigned long files_writeFile(const char* filename, const char* contentBuffer, unsigned long bufferSize) {
+    FileInfo info = files_getInfo(filename);
+    if (info.exists && info.isDirectory) {
+        return -1;
+    }
+    HANDLE file = CreateFile(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (GetLastError() != ERROR_ALREADY_EXISTS && GetLastError() != 0) {
+        printf("Unable to open file: %s\n", filename);
+        printf("Error code: %ld", GetLastError());
+        CloseHandle(file);
+        return -1;
+    }
+
+    unsigned long wroteCount;
+    if (!WriteFile(file, contentBuffer, bufferSize, &wroteCount, NULL)) {
+        printf("Unable to write file: %s\n", filename);
+        printf("Error code: %ld\n", GetLastError());
+        CloseHandle(file);
+        return -1;
+    }
+    CloseHandle(file);
+
+    return wroteCount;
 }
 
 #endif
--- src/common/files.h
+++ src/common/files.h
@@ -6,6 +6,8 @@
  * \brief A class containing information about a file
  */
 typedef struct FileInfo {
+    /** Equal to 1 if the file exists, else 0 */
+    int exists;
     /** Equal to 1 if the file is a directory */
     int isDirectory;
     /** File size in bytes. If the file is a directory, this is equal to 0 */
@@ -19,5 +21,37 @@ typedef struct FileInfo {
  * @return the information of the file with the given name
  */
 FileInfo files_getInfo(const char* filename);
+
+/**
+ * \brief Reads the given file
+ *
+ * An error can occur if :
+ *  - the given file is a directory
+ *  - the given file is protected against reading
+ *  - the given buffer is too small for the specified size
+ *
+ * \param filename The name of the file to read
+ * \param contentBuffer The buffer to store file content in
+ * \param bufferSize The size of the given buffer
+ *
+ * \return the actually read size or -1 if an error occurred
+ */
+unsigned long files_readFile(const char* filename, char* contentBuffer, unsigned long bufferSize);
+
+/**
+ * \brief Writes the given file
+ *
+ * An error can occur if :
+ *  - the given file is a directory
+ *  - the given file is protected against writing
+ *  - the given buffer is too small for the specified size
+ *
+ * \param filename The name of the file to write to
+ * \param contentBuffer The buffer to get file content from
+ * \param bufferSize The size of the given buffer
+ *
+ * \return the actually written size or -1 if an error occurred
+ */
+unsigned long files_writeFile(const char* filename, const char* contentBuffer, unsigned long bufferSize);
 
 #endif //C_CHAT_FILES_H
